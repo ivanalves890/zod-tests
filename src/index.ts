e54d3498d1de1c8fbefb42c445db4dea4d8be70d
Ivan Alves <ivan.alves22@hotmail.com>
2025-10-07 13:15:28 -0300

    feat: tested cart schema

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {
   type User,
 } from "./validators/user.validator.js";
 import { printResult } from "./utils/functions.js";
+import { cartSchema, type Cart } from "./validators/cart.validator.js";
 
 const userNames = [
   "João de Santo Cristo não tinha medo",
@@ -40,3 +41,17 @@ users.forEach((u) => {
 });
 
 console.log("\n");
+
+const cart: Cart = {
+  customerId: crypto.randomUUID(),
+  products: [
+    {
+      name: "Escova de dentes",
+      price: 15.63,
+      quantity: 1,
+      categories: ["Higiene", "Higiene Bucal"],
+    },
+  ],
+};
+
+printResult(cart, cartSchema);
--- src/validators/cart.validator.ts
+++ src/validators/cart.validator.ts
@@ -4,10 +4,18 @@ export const cartSchema = z.object({
   customerId: z.uuid(),
   products: z.array(
     z.object({
-      name: z.string().min(3),
-      price: z.number().min(0),
-      quantity: z.number().min(0),
-      categories: z.array(z.string()).min(1),
+      name: z
+        .string("Nome do produto deve ser tipo texto")
+        .min(3, "Nome do produto deve ter no mínimo 3 caracteres"),
+      price: z
+        .number("Preço deve ser tipo número")
+        .min(0, "Preço deve ser no mínimo 0"),
+      quantity: z
+        .number("Quantidade deve ser tipo número")
+        .min(0, "Quantidade deve ser no mínimo 0"),
+      categories: z
+        .array(z.string("Categoria deve ser do tipo texto"))
+        .min(1, "O produto deve ter no mínimo 1 categoria"),
     })
   ),
 });
